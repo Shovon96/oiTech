c23e7b460ebad291950fc8c9ba436089f366aafd
Shovon96 <fakhruddinshovon@gmail.com>
2023-11-26 01:14:59 +0600

    trending sort by upvote

--- src/Pages/HomePage/Trending.jsx
+++ src/Pages/HomePage/Trending.jsx
@@ -2,26 +2,43 @@ import { useQuery } from "@tanstack/react-query";
 import useAxiosPublic from "../../Hooks/useAxiosPublic";
 import SectionTitle from "../../Shared/SectionTitle";
 import ProductsCard from "../../Shared/ProductsCard";
+import { useState } from "react";
 
 const Trending = () => {
 
-    const axiosPublic = useAxiosPublic()
+    const axiosPublic = useAxiosPublic();
+    const [sort, setSort] = useState(false)
 
-    const { data: trending = [] } = useQuery({
-        queryKey: ['trendings'],
+    const { data: trending = [], refetch, isLoading } = useQuery({
+        queryKey: ['trendings', sort],
         queryFn: async () => {
-            const res = await axiosPublic.get('/trendings')
-            return res.data
+            if (sort) {
+                const res = await axiosPublic.get('/sort?sort=true')
+                return res.data
+            } else {
+                const res = await axiosPublic.get('/trendings')
+                return res.data
+            }
         }
     })
-    console.log(trending);
+    const handleSortUpvote = () => {
+        setSort(!sort)
+        refetch()
+    }
+    
+    if(isLoading){
+        return <div className="flex justify-center items-center h-[70vh]">Loding....</div>
+    }
 
     return (
         <>
-            <SectionTitle 
-            heading={"Trending Products"} 
-            title={"Elevate your digital experience with our innovative products, merging style and functionality seamlessly.Unleash possibilities with our advanced tech lineup, designed to redefine the way you live and work."}>
+            <SectionTitle
+                heading={"Trending Products"}
+                title={"Elevate your digital experience with our innovative products, merging style and functionality seamlessly.Unleash possibilities with our advanced tech lineup, designed to redefine the way you live and work."}>
             </SectionTitle>
+            <div className="flex justify-end mb-4">
+                <button onClick={handleSortUpvote} className="px-6 border">Sort By UpVote</button>
+            </div>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-12">
                 {
                     trending?.map(item =>
